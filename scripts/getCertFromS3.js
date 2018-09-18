f29e01349066520857ef3459d6d092ad5ef4910a
Ganimat Singh <ganimat.singh@f5.com>
2018-09-18 14:30:42 -0700

    Update get ssl cert from s3 script to retrive certs that are more than one directory deep

--- scripts/getCertFromS3.js
+++ scripts/getCertFromS3.js
@@ -17,6 +17,8 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 var q = require('q');
+var mkdirp = require('mkdirp');
+var path = '/config/ssl/ssl.key/';
 var uri;
 var params;
 var file;
@@ -25,12 +27,13 @@ if (process.argv.length <= 2) {
     console.log("Usage: " + __filename + " <ARN> (Format should be arn:aws:s3:::bucket_name/key_name)");
     process.exit(-1);
 }
- 
+
 uri = process.argv[2];
- 
+
 console.log('uri: ' + uri);
 
 if (!uri.startsWith('arn:aws:s3:::')) {
+    console.log("Invalid URI. URI should be an S3 arn");
     return q.reject(new Error("Invalid URI. URI should be an S3 arn."));
 }
 
@@ -42,6 +45,7 @@ parts = parts[1].split(/\/(.+)/);
 
 // length === 3 because splitting on just the first match leaves an empty string at the end
 if (parts.length !== 3) {
+    console.log("Invalid ARN. Format should be arn:aws:s3:::bucket_name/key_name");
     return q.reject(new Error("Invalid ARN. Format should be arn:aws:s3:::bucket_name/key_name"));
 }
 
@@ -53,8 +57,21 @@ params = {
     Key: key
 };
 
-console.log(bucket)
-console.log(key)
+console.log(bucket);
+console.log(key);
 
-file = require('fs').createWriteStream('/config/ssl/ssl.key/'+key);
+keyDirPath = key.split(/\//);
+
+if (keyDirPath.length > 1) {
+    for (var i = 0; i < keyDirPath.length - 1; i++) {
+        path += keyDirPath[i] + '/';
+    }
+    mkdirp(path, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+file = require('fs').createWriteStream(path + keyDirPath[keyDirPath.length - 1]);
 s3.getObject(params).createReadStream().pipe(file);
